21732f9136736c09a12fcf6351623735531349ab
Ryan LaBouve <imryanlabouve@gmail.com>
2019-01-01 16:44:18 -0600

    Initial components and mirage install

--- app/components/view-move/component.js
+++ app/components/view-move/component.js
@@ -1,65 +1,246 @@
 import Component from '@ember/component';
-import {computed, get} from '@ember/object';
+import {computed, get, set} from '@ember/object';
 
 const EXERCISE_MAP = {
   sq_variant: {
     name: 'Squat Variation',
+    exercises: [
+      {
+        name: 'Back Squat',
+      },
+      {
+        name: 'Goblet Squat',
+      },
+    ],
   },
   dl_variant: {
     name: 'Deadlift Variation',
+    exercises: [
+      {
+        name: 'Deadlift',
+      },
+      {
+        name: 'KettleBell Deadlift',
+      },
+    ],
   },
   sl_variant: {
     name: 'Single-leg Variation',
+    exercises: [
+      {
+        name: 'Lunges',
+      },
+      {
+        name: 'Single-leg Leg press',
+      },
+    ],
   },
   leg_curl: {
     name: 'Leg Curl',
+    exercises: [
+      {
+        name: 'Seated Leg-Curl',
+      },
+      {
+        name: 'Prone Leg-Curl',
+      },
+    ],
   },
   st_calf: {
-    name: 'Seated Calf',
+    name: 'Standing Calf',
+    exercises: [
+      {
+        name: 'Standing Calf Raise Machine',
+      },
+      {
+        name: 'Leg Press Calf Raise',
+      },
+      {
+        name: 'Smith Machine Calf Raise',
+      },
+      {
+        name: 'Dumb-Bell Calf Raise',
+      },
+    ],
   },
   hor_push: {
     name: 'Horizontal Push',
+    exercises: [
+      {
+        name: 'Bench Press',
+      },
+      {
+        name: 'Dumb-bell Press',
+      },
+      {
+        name: 'Inclined Bench Press',
+      },
+    ],
   },
   hor_pull: {
     name: 'Horizontal Pull',
+    exercises: [
+      {
+        name: 'Seated Cable Row',
+      },
+      {
+        name: 'Barbell Row',
+      },
+      {
+        name: 'Chest Supported Db Row',
+      },
+    ],
   },
   ver_push: {
     name: 'Vertical Push',
+    exercises: [
+      {
+        name: 'Overhead Press',
+      },
+      {
+        name: 'Standing Dumbbell Press',
+      },
+      {
+        name: 'Arnold Press',
+      },
+    ],
   },
   ver_pull: {
     name: 'Vertical Pull',
+    exercises: [
+      {
+        name: 'Lat-Pull Down',
+      },
+      {
+        name: 'Supinated Lat-Pull Down',
+      },
+      {
+        name: 'Chin up (chains or bands)',
+      },
+      {
+        name: 'Pull up (chains or bands)',
+      },
+    ],
   },
   biceps: {
     name: 'Biceps',
+    exercises: [
+      {
+        name: 'Curl',
+      },
+      {
+        name: 'Hammer Curl',
+      },
+      {
+        name: 'Preacher Curl',
+      },
+    ],
   },
   triceps: {
     name: 'Triceps',
+    exercises: [
+      {
+        name: 'Cable Pull-downs',
+      },
+      {
+        name: 'Skull Crusher',
+      },
+    ],
   },
   hh_variant: {
     name: 'Hip Hinge',
+    exercises: [
+      {
+        name: 'Barbell Hip Thrusts',
+      },
+      {
+        name: 'Barbell Glute Bridge',
+      },
+      {
+        name: 'Cable Pull Throughs',
+      },
+    ],
   },
   lp_variant: {
     name: 'Leg-press Variation',
+    exercises: [
+      {
+        name: 'Hack Squat',
+      },
+      {
+        name: 'Leg press',
+      },
+    ],
   },
   leg_ext: {
     name: 'Leg Extension',
+    exercises: [
+      {
+        name: 'Leg Extension',
+      },
+    ],
   },
   se_calf: {
     name: 'Seated Calf',
+    exercises: [
+      {
+        name: 'Stead Calf Raise Machine',
+      },
+    ],
   },
   dips: {
     name: 'Dips',
+    exercises: [
+      {
+        name: 'Dips',
+      },
+      {
+        name: 'Dip Machine',
+      },
+    ],
   },
   wt_b_ext: {
     name: 'Weighted Back Extension',
+    exercises: [
+      {
+        name: 'Weighted Back Extension',
+      },
+    ],
   },
   face_pull: {
     name: 'Face Pull',
+    exercises: [
+      {
+        name: 'High Face Pull',
+      },
+      {
+        name: 'Low Face Pull',
+      },
+    ],
+  },
+  inc_push: {
+    name: 'Inclined Push',
+    exercises: [
+      {
+        name: 'Inclined Barbell Bench',
+      },
+      {
+        name: 'Inclined Dumbbell Bench',
+      },
+    ],
   },
 };
 
 export default Component.extend({
+  rawMove: computed('move.code', function() {
+    return EXERCISE_MAP[get(this, 'move.code')];
+  }),
+
+  didInsertElement() {
+    set(this, 'selectedExercise', get(this, 'rawMove.exercises.firstObject'));
+  },
+
   name: computed('move.code', function() {
-    return EXERCISE_MAP[get(this, 'move.code')].name;
+    return get(this, 'rawMove').name;
   }),
 });
--- app/utils/programs.js
+++ app/utils/programs.js
@@ -1,11 +1,357 @@
-// const BB_D1 = {};
-// const BB_D2 = {};
-// const BB_D3 = {};
-// const BB_D4 = {};
-// const BB_D5 = {};
-//
-// const PLAN_BB_INTERMEDIATE_BLOCK_A = [BB_D1, BB_D2, BB_D3, BB_D4, BB_D5];
-//
+const PLAN_BB_ULTRA_NOVICE = {
+  weeks: 12,
+  week: [
+    {
+      name: 'Day 1, Lower',
+      moves: [
+        {
+          code: 'lp_variant',
+          sets: 3,
+          reps_low: 5,
+          reps_high: 8,
+          percent_rm: 80,
+          rpe: 8,
+        },
+        {
+          code: 'dl_variant',
+          sets: 2,
+          reps_low: 3,
+          reps_high: 5,
+          percent_rm: 80,
+          rpe: 8,
+        },
+        {
+          code: 'sl_variant',
+          sets: 3,
+          reps_low: 6,
+          reps_high: 8,
+          percent_rm: null,
+          rpe: 8,
+        },
+        {
+          code: 'st_calf',
+          sets: 3,
+          reps_low: 6,
+          reps_high: 8,
+          percent_rm: null,
+          rpe: 8,
+        },
+      ],
+    },
+    {
+      name: 'Day 2, Upper',
+      moves: [
+        {
+          code: 'hor_push',
+          sets: 3,
+          reps_low: 3,
+          reps_high: 5,
+          percent_rm: 85,
+          rpe: 8,
+        },
+        {
+          code: 'hor_pull',
+          sets: 3,
+          reps_low: 4,
+          reps_high: 6,
+          percent_rm: null,
+          rpe: 8,
+        },
+        {
+          code: 'ver_push',
+          sets: 2,
+          reps_low: 6,
+          reps_high: 8,
+          percent_rm: 80,
+          rpe: 8,
+        },
+        {
+          code: 'ver_pull',
+          sets: 2,
+          reps_low: 6,
+          reps_high: 8,
+          percent_rm: null,
+          rpe: 8,
+        },
+      ],
+    },
+    {
+      name: 'Day 3, Lower',
+      moves: [
+        {
+          code: 'hh_variant',
+          sets: 3,
+          reps_low: 6,
+          reps_high: 8,
+          percent_rm: null,
+          rpe: 8,
+        },
+        {
+          code: 'lp_variant',
+          sets: 3,
+          reps_low: 6,
+          reps_high: 8,
+          percent_rm: null,
+          rpe: 8,
+        },
+        {
+          code: 'leg_ext',
+          sets: 3,
+          reps_low: 8,
+          reps_high: 12,
+          percent_rm: null,
+          rpe: 8,
+        },
+        {
+          code: 'leg_curl',
+          sets: 3,
+          reps_low: 8,
+          reps_high: 12,
+          percent_rm: null,
+          rpe: 8,
+        },
+        {
+          code: 'se_calf',
+          sets: 4,
+          reps_low: 12,
+          reps_high: 15,
+          percent_rm: null,
+          rpe: 8,
+        },
+      ],
+    },
+    {
+      name: 'Day 4, Push',
+      moves: [
+        {
+          code: 'hor_push',
+          sets: 3,
+          reps_low: 8,
+          reps_high: 10,
+          percent_rm: 80,
+          rpe: 8,
+        },
+        {
+          code: 'hor_pull',
+          sets: 3,
+          reps_low: 8,
+          reps_high: 12,
+          percent_rm: null,
+          rpe: 8,
+        },
+        {
+          code: 'ver_push',
+          sets: 2,
+          reps_low: 10,
+          reps_high: 12,
+          percent_rm: null,
+          rpe: 8,
+        },
+        {
+          code: 'ver_pull',
+          sets: 2,
+          reps_low: 10,
+          reps_high: 12,
+          percent_rm: null,
+          rpe: 8,
+        },
+        {
+          code: 'triceps',
+          sets: 2,
+          reps_low: 10,
+          reps_high: 12,
+          percent_rm: null,
+          rpe: 8,
+        },
+        {
+          code: 'biceps',
+          sets: 2,
+          reps_low: 12,
+          reps_high: 15,
+          percent_rm: null,
+          rpe: 8,
+        },
+      ],
+    },
+  ],
+};
+const PLAN_BB_NOVICE = {
+  weeks: 12,
+  week: [
+    {
+      name: 'Day 1, Lower',
+      moves: [
+        {
+          code: 'sq_variant',
+          sets: 3,
+          reps_low: 3,
+          reps_high: 5,
+          percent_rm: 80,
+          rpe: 8,
+        },
+        {
+          code: 'dl_variant',
+          sets: 3,
+          reps_low: 3,
+          reps_high: 5,
+          percent_rm: 80,
+          rpe: 8,
+        },
+        {
+          code: 'sl_variant',
+          sets: 3,
+          reps_low: 6,
+          reps_high: 8,
+          percent_rm: null,
+          rpe: 8,
+        },
+        {
+          code: 'st_calf',
+          sets: 3,
+          reps_low: 6,
+          reps_high: 8,
+          percent_rm: null,
+          rpe: 8,
+        },
+      ],
+    },
+    {
+      name: 'Day 2, Upper',
+      moves: [
+        {
+          code: 'hor_push',
+          sets: 3,
+          reps_low: 3,
+          reps_high: 5,
+          percent_rm: 85,
+          rpe: 8,
+        },
+        {
+          code: 'hor_pull',
+          sets: 3,
+          reps_low: 4,
+          reps_high: 6,
+          percent_rm: null,
+          rpe: 8,
+        },
+        {
+          code: 'ver_push',
+          sets: 2,
+          reps_low: 6,
+          reps_high: 8,
+          percent_rm: 80,
+          rpe: 8,
+        },
+        {
+          code: 'ver_pull',
+          sets: 2,
+          reps_low: 6,
+          reps_high: 8,
+          percent_rm: null,
+          rpe: 8,
+        },
+      ],
+    },
+    {
+      name: 'Day 3, Lower',
+      moves: [
+        {
+          code: 'hh_variant',
+          sets: 3,
+          reps_low: 6,
+          reps_high: 8,
+          percent_rm: null,
+          rpe: 8,
+        },
+        {
+          code: 'lp_variant',
+          sets: 3,
+          reps_low: 6,
+          reps_high: 8,
+          percent_rm: null,
+          rpe: 8,
+        },
+        {
+          code: 'leg_ext',
+          sets: 3,
+          reps_low: 8,
+          reps_high: 12,
+          percent_rm: null,
+          rpe: 8,
+        },
+        {
+          code: 'leg_curl',
+          sets: 3,
+          reps_low: 8,
+          reps_high: 12,
+          percent_rm: null,
+          rpe: 8,
+        },
+        {
+          code: 'se_calf',
+          sets: 4,
+          reps_low: 12,
+          reps_high: 15,
+          percent_rm: null,
+          rpe: 8,
+        },
+      ],
+    },
+    {
+      name: 'Day 4, Push',
+      moves: [
+        {
+          code: 'hor_push',
+          sets: 3,
+          reps_low: 8,
+          reps_high: 10,
+          percent_rm: 80,
+          rpe: 8,
+        },
+        {
+          code: 'hor_pull',
+          sets: 3,
+          reps_low: 8,
+          reps_high: 12,
+          percent_rm: null,
+          rpe: 8,
+        },
+        {
+          code: 'inc_push',
+          sets: 2,
+          reps_low: 10,
+          reps_high: 12,
+          percent_rm: null,
+          rpe: 8,
+        },
+        {
+          code: 'ver_pull',
+          sets: 2,
+          reps_low: 10,
+          reps_high: 12,
+          percent_rm: null,
+          rpe: 8,
+        },
+        {
+          code: 'triceps',
+          sets: 2,
+          reps_low: 10,
+          reps_high: 12,
+          percent_rm: null,
+          rpe: 8,
+        },
+        {
+          code: 'biceps',
+          sets: 2,
+          reps_low: 12,
+          reps_high: 15,
+          percent_rm: null,
+          rpe: 8,
+        },
+      ],
+    },
+  ],
+};
 const PLAN_BB_INTERMEDIATE = {
   weeks: 12,
   week: [
@@ -214,7 +560,7 @@ const RYANS_PROGRAM = {
 const ROBYNS_PROGRAM = {
   name: "Robyn's Program",
   slug: 'robyns-program',
-  plan: PLAN_BB_INTERMEDIATE,
+  plan: PLAN_BB_ULTRA_NOVICE,
 };
 
 const PROGRAMS = [RYANS_PROGRAM, ROBYNS_PROGRAM];
