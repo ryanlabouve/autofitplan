aa08e97575e6b12665fd795d2d3bd847084ea609
Ryan LaBouve <imryanlabouve@gmail.com>
2019-06-16 00:26:17 -0500

    Slightly improve set ux

--- app/components/view-move/single-set/component.js
+++ app/components/view-move/single-set/component.js
@@ -1,5 +1,13 @@
 import Component from '@ember/component';
+import {set} from '@ember/object';
 
 export default Component.extend({
   tagName: '',
+  changeShowing(name) {
+    if (this.currentlyEditing === name) {
+      set(this, 'currentlyEditing', null);
+    } else {
+      set(this, 'currentlyEditing', name);
+    }
+  },
 });
